60a07ac420bd3679b5c9153b8f315b532dec2209
yag <yaghmurassen@carlili.fr>
2024-04-09 17:35:15 +0200

    WIP Blog List responsive

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -9,55 +9,54 @@ const BlogRollTemplate = (props) => {
   const { edges: posts } = props.data.allMarkdownRemark;
 
   return (
-    <div>
+    <div className="blog-container">
       {posts &&
         posts.map(({ node: post }) => (
-          <div className="is-parent column is-6" key={post.id}>
-            <article
-              className={`blog-list-item tile is-child box notification ${
-                post.frontmatter.featuredpost ? "is-featured" : ""
-              }`}
-            >
-              <header>
-                {post?.frontmatter?.featuredimage && (
-                  <div className="featured-thumbnail">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: post.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        width:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.width,
-                        height:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.height,
-                      }}
-                    />
-                  </div>
-                )}
-                <p className="post-meta">
-                  <Link
-                    className="title has-text-primary is-size-4"
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <span className="subtitle is-size-5 is-block">
-                    {post.frontmatter.date}
-                  </span>
-                </p>
-              </header>
-              <p>
+          <article
+            key={post.id}
+            className={`blog-list-item max-xs:p-4 p-12 grid max-md2:grid-cols-1 grid-cols-3/1 gap-8 ${
+              post.frontmatter.featuredpost ? "is-featured" : ""
+            }`}
+          >
+            <header className="m-auto">
+              {post?.frontmatter?.featuredimage && (
+                <div className="featured-thumbnail">
+                  <PreviewCompatibleImage
+                    imageInfo={{
+                      image: post.frontmatter.featuredimage,
+                      alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                      width:
+                        post.frontmatter.featuredimage.childImageSharp
+                          .gatsbyImageData.width,
+                      height:
+                        post.frontmatter.featuredimage.childImageSharp
+                          .gatsbyImageData.height,
+                    }}
+                  />
+                </div>
+              )}
+            </header>
+            <div>
+              <p className="post-meta italic text-xl mb-8 max-xs:text-lg max-xs:mb-6">
+                <Link
+                  className="title has-text-primary is-size-4"
+                  to={post.fields.slug}
+                >
+                  {post.frontmatter.title}
+                </Link>
+                <span> &bull; </span>
+                <span className="text-xs">{post.frontmatter.date}</span>
+              </p>
+              <p className="text-sm text-justify">
                 {post.excerpt}
                 <br />
                 <br />
-                <Link className="button" to={post.fields.slug}>
+                <Link className="button font-bold" to={post.fields.slug}>
                   Lire la suite →
                 </Link>
               </p>
-            </article>
-          </div>
+            </div>
+          </article>
         ))}
     </div>
   );
@@ -95,9 +94,9 @@ export default function BlogRoll() {
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
-                        width: 120
+                        width: 500
                         quality: 100
-                        layout: CONSTRAINED
+                        # layout: FULL_WIDTH
                       )
                     }
                   }
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -9,10 +9,9 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <section className="section-blog">
-          <div className="container-fluid">
-            <BlogRoll />
-          </div>
+        <section className="container-fluid">
+          <h1 className="text-4xl font-bold mx-12 mt-28 mb-16">Articles</h1>
+          <BlogRoll />
         </section>
       </Layout>
     );
